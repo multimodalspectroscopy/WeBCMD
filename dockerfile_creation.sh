4cb6aad2a1cbb13fac668b25cc412e4a42802f90
Josh Buckland <ucbpjru@ucl.ac.uk>
2017-06-16 14:01:17 +0100

    changed to write over existing Dockerfile, not append

--- dockerfile_creation.sh
+++ dockerfile_creation.sh
@@ -2,4 +2,4 @@
 # Script to create a Dockerfile that will ensure the Gitclone command runs every time
 
 DATE=`date`
-sed -e "s/\${{date}}/$DATE/"  dockerfile.template >> Dockerfile
+sed -e "s/\${{date}}/$DATE/"  dockerfile.template > Dockerfile
